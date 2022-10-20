9eb20fba78a3c3e72544cc50fbca2419571ec714
Vignesh Ashokan <vigneshgashokan@gmail.com>
2022-10-20 09:18:10 -0600

    WIP: useState, useEffect, useRef

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,57 @@
-import React from 'react';
+import React, { useState } from "react";
+import Accordion from "./Accordion";
+import Search from "./Search";
+import Dropdown from "./Dropdown";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS libray among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
 
 function App() {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
-    <h1>Hello World!!</h1>
+    <div>
+      {/* <Accordion items={items} /> */}
+      {/* <Search /> */}
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
+    </div>
   );
 }
 
